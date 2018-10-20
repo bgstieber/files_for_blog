0c8bcf9db62891689ee2d22b5729d4814172248f
Brad Stieber <bgstieber@gmail.com>
2018-10-20 15:05:54 -0500

    added new file

---
+++ scrape-wsga-results/01scrape_wsga_results.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive'}
+
+def url_to_bs(url):
+    request_url = requests.get(url = url,
+                               headers = headers)
+    
+    return BeautifulSoup(request_url.content)
