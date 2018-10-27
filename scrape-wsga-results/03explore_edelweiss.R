41c6503bca158391435de4698ebb5d730910d89a
Brad Stieber <bgstieber@gmail.com>
2018-10-27 10:48:01 -0500

    cleaned and commented; bit of modeling work

--- scrape-wsga-results/03explore_edelweiss.R
+++ scrape-wsga-results/03explore_edelweiss.R
@@ -1,12 +1,13 @@
 library(tidyverse)
-
+library(broom)
+theme_set(theme_bw())
+# data manipulation
 results <- read_csv("data//edelweiss_data.csv")
 course <- read_csv("data//edelweiss_course_data.csv")
-
+# wide to long (tidy)
 results_long <- results %>%
   gather(hole, score, starts_with('hole'))
-
-
+# clean up course information data
 course2 <- course %>%
   select(hole = `hole#`,
          par,
@@ -14,16 +15,11 @@ course2 <- course %>%
   filter(hole != 'Totals') %>%
   distinct() %>%
   mutate(hole = paste0('hole_', hole))
-
+# join to get course information
 results_long2 <- results_long %>%
   inner_join(course2) %>%
   mutate(score_to_par = score - par)
-
-results_long2 %>%
-  group_by(hole) %>%
-  summarise(score_to_par = mean(score_to_par))
-
-
+# average score to par by hole
 results_long2 %>%
   group_by(hole, par) %>%
   summarise(score_to_par = mean(score_to_par)) %>%
@@ -34,3 +30,12 @@ results_long2 %>%
 # two holes played ~even par
 fit1 <- lm(score_to_par ~ -1 + hole, data = results_long2)
 summary(fit1)
+fit1_info <- tidy(fit1, conf.int = TRUE)
+
+fit1_info %>%
+  mutate(term = substring(term, 5, 999)) %>%
+  inner_join(course2, by = c('term' = 'hole')) %>%
+  ggplot(aes(reorder(term, estimate),estimate))+
+  geom_linerange(aes(ymin = conf.low, ymax = conf.high), alpha = 0.5)+
+  geom_point(aes(colour = factor(par)), size = 2)+
+  coord_flip()
