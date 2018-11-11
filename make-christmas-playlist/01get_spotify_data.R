8a9dcac748e4b68351925188af7fabf227d167af
Brad Stieber <bgstieber@gmail.com>
2018-11-11 08:39:36 -0600

    more exploratory analysis of PC

--- make-christmas-playlist/01get_spotify_data.R
+++ make-christmas-playlist/01get_spotify_data.R
@@ -2,10 +2,9 @@ library(spotifyr)
 library(tidyverse)
 library(scales)
 library(caret)
+library(ggrepel)
 theme_set(theme_bw())
 # connect
-Sys.setenv(SPOTIFY_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxx')
-Sys.setenv(SPOTIFY_CLIENT_SECRET = 'xxxxxxxxxxxxxxxxxxxxx')
 access_token <- get_spotify_access_token()
 
 playlist_info <- 'spotify:user:12102534356:playlist:6IWeDaYa1NQLzPWWcSh2GL'
@@ -80,6 +79,10 @@ spotify_holiday_features2_scaled <- predict(pre_proc,
 # fit principal components (dimensionality reduction) on playlist
 prco_holiday <- prcomp(holiday_playlist_features2_scaled[,-1])
 
+pc_rotation <- prco_holiday$rotation %>%
+  as_data_frame() %>%
+  mutate(variable = row.names(prco_holiday$rotation))
+
 training_pca <- predict(prco_holiday) %>%
   as_data_frame() %>%
   mutate(type = 'training')
@@ -92,8 +95,20 @@ testing_pca <- predict(prco_holiday,
 full_pca <- training_pca %>%
   bind_rows(testing_pca)
 
+pc_rotation %>%
+  gather(pc, value, -variable) %>%
+  filter(pc %in% paste0('PC', 1:4)) %>%
+  ggplot(aes(pc, variable, fill = value))+
+  geom_tile(colour = 'grey20')+
+  scale_fill_gradient2()
+
+full_pca %>%
+  filter(type == 'training') %>%
+  mutate(song = holiday_playlist$track_name) %>%
+  ggplot(aes(PC1, PC2, colour = type))+
+  geom_point()+
+  geom_text_repel(aes(label = song))
 
 full_pca %>%
   ggplot(aes(PC1, PC2, colour = type))+
   geom_point()
-
