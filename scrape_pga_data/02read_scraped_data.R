d8d1c2548a7a183a870b5c5ac5b63f1347ac1d90
Brad Stieber <bgstieber@gmail.com>
2019-06-08 06:27:40 -0500

    changes to logic

--- scrape_pga_data/02read_scraped_data.R
+++ scrape_pga_data/02read_scraped_data.R
@@ -1,7 +1,20 @@
 library(tidyverse)
 
-all_pga_data <- file.path("data", dir("data/")) %>%
-  map(read_csv)
+file_names <- dir("data//")
 
+file_groups <- file_names %>%
+  gsub(pattern = "_[0-9]{4}.[a-z]{3}",
+       replacement = "",
+       x = .) %>%
+  unique()
+
+file_paths <- file.path("data", dir("data/"))
+
+all_pga_data <- file_groups %>%
+  map(~file_paths[grepl(pattern = .x, x = file_paths)]) %>%
+  set_names(file_groups) %>%
+  map_df(~as_tibble(.x), .id = 'group') %>%
+  rowwise() %>%
+  mutate(data = map(value, read_csv))
 
 
