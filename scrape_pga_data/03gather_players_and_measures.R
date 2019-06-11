36c5c39a1b2e8ddaef3a8b71b092b6d67705bc75
Brad Stieber <bgstieber@gmail.com>
2019-06-10 20:11:02 -0500

    added putting

--- scrape_pga_data/03gather_players_and_measures.R
+++ scrape_pga_data/03gather_players_and_measures.R
@@ -20,11 +20,21 @@ money <- full_data$pct_total_money_won_FULL.csv %>%
   mutate(money_won = parse_number(`OFFICIAL MONEY WON`))
 
 money_scaled <-money %>%
-  select(year, `PLAYER NAME`, money_won) %>%
+  select(year, `PLAYER NAME`, money_won, EVENTS) %>%
   group_by(year) %>%
-  mutate(scaled_money = scale(log1p(money_won)))
+  mutate(scaled_money = scale(log1p(money_won))) %>%
+  ungroup()
 
 sg_tee_to_green <- full_data$sg_tee_to_green_FULL.csv
+
+sg_tee_to_green_minimal <- sg_tee_to_green %>%
+  select(year, `PLAYER NAME`,
+         'SG:TTG' = AVERAGE, `SG:OTT`, `SG:APR`,`SG:ARG`)
+
+sg_putting <- full_data$sg_putting_FULL.csv
+
+sg_putting_minimal <- sg_putting %>%
+  select(year, `PLAYER NAME`, 'SG:PUTT' = AVERAGE)
 
 yoy_performance <- sg_tee_to_green %>%
   select(year, `PLAYER NAME`, AVERAGE) %>%
@@ -40,3 +50,15 @@ yoy_performance %>%
   arrange(desc(abs(change_from_last_year))) %>%
   top_n(25, abs(change_from_last_year))
 
+
+
+money_and_sg <- money_scaled %>%
+  inner_join(sg_tee_to_green_minimal,
+             by = c("year", "PLAYER NAME")) %>%
+  inner_join(sg_putting_minimal,
+             by = c("year", "PLAYER NAME"))
+
+fit1 <- lm(scaled_money ~ -1 + bs(`SG:TTG`, df = 4), data = money_and_sg)
+
+
+money_and_sg$pred <- predict(fit1)
