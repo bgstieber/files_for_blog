f4922fea6cb1d74eea10a957b26b2d1352b13021
Brad Stieber <bgstieber@gmail.com>
2019-10-26 07:45:10 -0500

    more clean up

--- madison-fish-fy/03clean_data_for_analysis.py
+++ madison-fish-fy/03clean_data_for_analysis.py
@@ -10,18 +10,23 @@ def clean_review(x):
 
 fish_data = pd.read_csv('full_review_data.csv')
 
-
-
 for i in range(fish_data.shape[0]):
     fish_data.review_date[i] = convert_date(fish_data.review_date[i])
     fish_data.review[i] = clean_review(str(fish_data.review[i]))
+
+len_review = [len(r) for r in fish_data.review]
+
+fish_data = fish_data.assign(review_length = len_review)    
     
-    
-fish_data.polarity = 0
-fish_data.subjectivity = 0
+polarity = []
+subjectivity = []
 
 for i in range(fish_data.shape[0]):
     tb_review = TextBlob(fish_data.review[i]).sentiment
     
-    fish_data.polarity[i] = tb_review[0]
-    fish_data.subjectivity[i] = tb_review[1]
+    polarity.append(tb_review[0])
+    subjectivity.append(tb_review[1])
+    
+fish_data = fish_data.assign(polarity = polarity)
+fish_data = fish_data.assign(subjectivity = subjectivity)
+
