6472241762fb8e4702a067ebd39b19b21532d441
Brad Stieber <bgstieber@gmail.com>
2021-01-01 12:28:46 -0600

    rmd creation

--- goodreads-data-analysis/01exploratory_analysis.R
+++ goodreads-data-analysis/01exploratory_analysis.R
@@ -12,21 +12,22 @@ books_2020 <- goodreads_data %>%
                              `Number of Pages`),
          rating_diff = `My Rating` - `Average Rating`,
          publish_year = coalesce(`Original Publication Year`,
-                                  `Year Published`))
+                                  `Year Published`)) %>%
+  rename(date_read = `Date Read`)
 
 
 # timing of books
 books_2020 %>%
-  count(`Date Read`) %>%
+  count(date_read) %>%
   mutate(cumulative_books = cumsum(n)) %>%
-  ggplot(aes(`Date Read`, cumulative_books))+
+  ggplot(aes(date_read, cumulative_books))+
   geom_line()+
   geom_point()
 # cumulative pages read
 books_2020 %>%
-  arrange(`Date Read`) %>%
+  arrange(date_read) %>%
   mutate(cumulative_pages = cumsum(page_count)) %>%
-  ggplot(aes(`Date Read`, cumulative_pages))+
+  ggplot(aes(date_read, cumulative_pages))+
   geom_line()+
   geom_point()
 # my rating vs average rating
@@ -42,14 +43,25 @@ books_2020 %>%
 
 
 books_2020 %>%
-  ggplot(aes(`Date Read`, `Average Rating`))+geom_point()
+  ggplot(aes(date_read, `Average Rating`))+geom_point()
 
 
 books_2020 %>%
-  ggplot(aes(`Date Read`, rating_diff))+
+  ggplot(aes(date_read, rating_diff))+
   geom_point()
 
 
 books_2020 %>%
-  ggplot(aes(`Date Read`, page_count))+
+  ggplot(aes(date_read, page_count))+
   geom_point()
+
+summary_by_date <- books_2020 %>%
+  group_by(date_read) %>%
+  summarise(pages = sum(page_count)) %>%
+  bind_rows(tibble(date_read = as.Date("2020-01-01"),
+                   pages = 0)) %>%
+  arrange(date_read) %>%
+  mutate(previous_date = lag(date_read)) %>%
+  mutate(days_since_last_book = as.numeric(difftime(
+    date_read, previous_date, units = "days"
+  )))
