d7d5c901ec3b1923f8d68f667b8b68ce037efe7d
Brad Stieber <bgstieber@gmail.com>
2021-02-14 17:25:51 -0600

    mapping

--- election-map-2020/01exploratory.R
+++ election-map-2020/01exploratory.R
@@ -6,6 +6,24 @@ library(maps)
 library(readxl)
 library(httr)
 
+
+# lat lon data for plotting
+special_fips <- c(12091L, 22099L, 37053L, 48167L, 51001L, 
+                  53053L, 53055L)
+county_map_with_fips <- county.fips %>% 
+  separate(polyname, c('region', 'subregion'), ',') %>%
+  mutate(subregion = ifelse(fips %in% special_fips,
+                            stringi::stri_split_fixed(subregion,
+                                                      ":",
+                                                      n=1,
+                                                      tokens_only = TRUE) %>%
+                              unlist,
+                            subregion)) %>%
+  unique() %>%
+  inner_join(map_data('county')) %>%
+  mutate(county_fips = ifelse(nchar(fips) == 4,
+                              paste0("0", fips), fips))
+
 # download covid data from nyt
 nyt_c19_1 <- "https://github.com/nytimes/covid-19-data"
 nyt_c19_2 <- "/blob/master/us-counties.csv?raw=true"
@@ -75,6 +93,7 @@ full_data <- e2020 %>%
 
 # data work, column creation, then output a final master data set
 model_data <- full_data %>%
+  inner_join(tibble(state_name = state.name, state_abbr = state.abb)) %>%
   select(-cases, -area_miles_squared, -pop_total, -pop_white,
          -total_votes, -diff, -per_gop, -per_dem, -per_point_diff) %>%
   mutate_at(c("oth_2012", "dem_2012", "gop_2012"),
@@ -97,7 +116,8 @@ simple_model <- glm(cbind(votes_dem, votes_gop) ~
                       pct_edu+
                       pct_white+
                       dem_2016+
-                      dem_shift_16_12,
+                      dem_shift_16_12+
+                      state_abbr,
                     data = model_data,
                     family = 'binomial',
                     na.action = na.exclude)
@@ -109,3 +129,10 @@ model_data$pred <- predict(simple_model, type = "response")
 model_data$actual_minus_pred <- model_data$actual - model_data$pred
 
 
+model_data %>%
+  inner_join(county_map_with_fips) %>%
+  ggplot(aes(long, lat, group = group, fill = actual_minus_pred))+
+  geom_polygon(colour = rgb(0.9,0.9,0.9, .1))+
+  geom_polygon(data = map_data('state'), fill = NA, colour = 'white')+
+  coord_map()+
+  scale_fill_gradient2()
